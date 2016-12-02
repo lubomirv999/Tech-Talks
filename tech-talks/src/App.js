c80903bf7fed4b60ec99c834db4885470ae388c3
Mirela Damyanova <mdamyanova181@gmail.com>
2016-12-02 12:50:12 +0200

    added Footer and Header and attach them to the main App

--- tech-talks/src/App.js
+++ tech-talks/src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+
+import NavigationBar from './Components/NavigationBar';
+import Footer from './Components/Footer';
+
+import KinveyRequester from './KinveyRequester';
+import $ from 'jquery';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <header>
+          // TODO: Initialize the props of nav bar (call functions and bind)
+          <NavigationBar/>
+        </header>
+        <main id='main'></main>
+        <Footer/>
       </div>
     );
   }
---
+++ tech-talks/src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                &copy; 2016 - Tech Talk (Software University JS Applications Project)
+            </footer>
+        );
+    }
+}
