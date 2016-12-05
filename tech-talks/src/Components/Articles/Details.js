efb6b7573728cc55de75a775a4897fe327cb8ed2
Mirela Damyanova <mdamyanova181@gmail.com>
2016-12-05 16:43:31 +0200

    added Comments

--- tech-talks/src/Components/Articles/Details.js
+++ tech-talks/src/Components/Articles/Details.js
@@ -2,24 +2,36 @@ import React, {Component} from 'react';
 import {loadArticleDetails, loadUsersDetails} from '../../Models/article';
 import ArticleControls from './ArticleControls';
 import './Details.css';
+import { addComment, loadArticleComments } from '../../Models/comment'
+import Comment from '../Comments/Comments'
 
 export default class Details extends Component {
     constructor(props) {
         super(props);
-        this.state ={
+        this.state = {
             title: '',
             articleContent: '',
             owner: '',
             canEdit: false,
-            ownTeam: sessionStorage.getItem('articleId') === this.props.params.articleId
+            ownTeam: sessionStorage.getItem('articleId') === this.props.params.articleId,
+            comments: [],
         };
 
         this.onLoadSuccess = this.onLoadSuccess.bind(this);
         this.onUsersSuccess = this.onUsersSuccess.bind(this);
+        this.onCommentsLoadSuccess = this.onCommentsLoadSuccess.bind(this);
+        this.onSubmitHandler = this.onSubmitHandler.bind(this);
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+        this.statusChange = this.statusChange.bind(this);
+    }
+
+    statusChange(response) {
+        this.context.router.push('/');
     }
 
     componentDidMount() {
         loadArticleDetails(this.props.params.articleId, this.onLoadSuccess);
+        loadArticleComments(this.props.params.articleId, this.onCommentsLoadSuccess);
     }
 
     onLoadSuccess(response) {
@@ -40,6 +52,24 @@ export default class Details extends Component {
         });
     }
 
+    onCommentsLoadSuccess(response) {
+        console.log(response);
+        this.setState({comments: response})
+    }
+
+    onSubmitHandler(event) {
+        event.preventDefault();
+        let userId = sessionStorage.getItem('userId');
+        let itemId = this.props.params.productId;
+        let username = sessionStorage.getItem('username');
+        addComment(this.state.text, userId, itemId, username, this.componentDidMount())
+    }
+
+    onChangeHandler(event) {
+        this.setState({text: event.target.value})
+    }
+
+
     render() {
         let title = 'Article';
         if (this.state.title !== '') {
@@ -54,10 +84,22 @@ export default class Details extends Component {
                 <span className="spanner">Content</span>
                 <p>{this.state.articleContent || 'No description'}</p>
                 <span className="spanner">Owner - {this.state.owner}</span>
+                <ul>
+                    {this.state.comments.map((comment, index) => {
+                        return (
+                            <li key={index}>{comment.text + ' posted by: ' + comment.username}</li>
+                        )
+                    })}
+                </ul>
                 <ArticleControls
                     articleId={this.props.params.articleId}
                     canEdit={this.state.canEdit}
                     ownTeam={this.state.ownTeam}
+                />
+                <Comment
+                    text={this.state.text}
+                    onChangeHandler={this.onChangeHandler}
+                    onSubmitHandler={this.onSubmitHandler}
                 />
             </div>
         )
