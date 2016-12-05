d3a7186b9840a5df452b1211f7561e97b1d30a1e
Mirela Damyanova <mdamyanova181@gmail.com>
2016-12-05 15:38:56 +0200

    added text on About Page

--- tech-talks/src/Components/About/AboutPage.js
+++ tech-talks/src/Components/About/AboutPage.js
@@ -5,6 +5,10 @@ export default class About extends Component {
         return (
             <div>
                 <h1>About</h1>
+                <p>Tech Talks is blog about technologies and people who like them. You can
+                register and view articles from other people and write your own. Also you can post comments on all the articles. Tech Talks is our Single Page Application Project for the course JS Applications of module JS Core in Software University.
+                Our team is from four young enthusiast programmers - Martin, Lyubomir, Mirela and Alexander. We were
+                using JavaScript with React JS.</p>
             </div>
         )
     }
